309d74ac63fdebd4e854c3681a183ba9bcafea29
tristinfriend <tristin.friend@maine.edu>
2024-09-26 16:12:08 -0400

    Wrote 15 Questions

---
+++ quiz.py
@@ -0,0 +1,79 @@
+'''
+COS 121
+Homework #3
+By Tristin Friend
+Due 09/30/2024
+'''
+
+# Begining of quiz
+print("Welcome to your history quiz! This quiz will consist of 15 questions. Are you ready? ")
+
+
+print("Question 1: What is the shorest war in recorded history?")
+# The Anglo-Zanibar War of 1896, lasting only 30 minutes
+
+print("Question 2: What was the first living creature to enter space?")
+# It was a dog named Laika. 
+
+print("Question 3: Who was the longest ruling monarch in Enlish history?")
+# Queen Elizabeth II, ruling for 66 years. 
+
+print("Question 4: Which famous Egyptian leader was actually Greek?")
+# Cleopatra
+
+print("Question 5: In the British colony of Jamestown, what was the their greatest cashcrop?")
+# tabacco 
+
+print("Question 6: The Declaration of Independence was sign on _________.")
+# July 4, 1776
+print("A: October 4, 1786")
+print("B: July 4, 1776")
+print("C: Febuary 6, 1779") 
+print("D: March 8, 1772") 
+user_response = input(": July 4, 1776")
+
+
+print("Question 7: __________ is the strong sense of loyalty to a state or section instead of a country.")
+# Sectionalism
+print("A: Popular Sovereignty")
+print("B: Manifest Destiny")
+print("C: Unalienable Rights")
+print("D: Sectionalism")
+
+print("Question 8: _____________ were people who opposed the Constitution.")
+# Antifederalists
+print("A: Federalists")
+print("B: Republicans")
+print("C: Antifederalists")
+print("D: Tories") 
+
+print("Question 9: The ___________ was one of the biggest influences on the Constitution.")
+# Magna Carta
+print("A: Declaration of Independence")
+print("B: Magna Carta")
+print("C: Articles of Confederation")
+print("D: Monroe Docterine") 
+
+print("Question 10: After King ____________ refused the Olive Branch Petition, the colonies finally broke away from England.")
+# George III
+print("A: George III")
+print("B: George Washington") 
+print("C: Alfred")
+print("D: Peter") 
+
+print("Question 11: Famous philospher __________ personally taught Alexander the Great.")
+# Aristotle
+
+print("Question 12: __________ cities were named after Alexander the Great.")
+# 70
+
+print("Question 13: The __________ dynasty in China outlawed sea trade.")
+# Ming Dynasty
+
+print("Question 14: _________ founded the Akkadian Empire.")
+# Sargon the Great
+
+print("Question 15: The Kingdom of _________ gave early rise to the Abrahamic religion.")
+# Israel
+
+# End of quiz
